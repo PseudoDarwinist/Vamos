82ab1eb452d5a5028fad06a127870f3dddab6268
Chetan Singh <heychetansingh@gmail.com>
2025-02-28 17:23:56 +0530

    Initial Commit

---
+++ Vamos/VamosApp.swift
@@ -0,0 +1,17 @@
+//
+//  VamosApp.swift
+//  Vamos
+//
+//  Created by Chetan Singh on 2025-02-28.
+//
+
+import SwiftUI
+
+@main
+struct VamosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
